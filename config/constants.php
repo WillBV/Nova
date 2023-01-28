be6e790a42df142f03b560fc1220f9c0560d170c
Will <willbv@hotmail.co.uk>
2023-01-28 11:06:20 +0000

    Update versions in composer

--- config/constants.php
+++ config/constants.php
@@ -1,4 +1,4 @@
 <?php
 
-define('NOVA_BASE_PATH', dirname(__DIR__));
-define('NOVA_VENDOR_PATH', NOVA_BASE_PATH.'/vendor');
+define("NOVA_BASE_PATH", dirname(__DIR__));
+define("NOVA_VENDOR_PATH", NOVA_BASE_PATH . "/vendor");
